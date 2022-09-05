2cb6548b19f3affae2829fa650feb86dd69b9347
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-05 20:14:15 -0300

    alterações nas queries pela mudança do banco (postgresql > mysql)

--- minhas-financas.sql
+++ minhas-financas.sql
@@ -1,30 +1,23 @@
-
--- DROP DATABASE minhasfinancas;
-
-CREATE DATABASE minhasfinancas;
-
-CREATE SCHEMA financas;
-
-DROP table financas.lancamento;
-
-CREATE TABLE financas.usuario
+CREATE TABLE usuario
 (
-  id bigserial NOT NULL PRIMARY KEY,
+  id int auto_increment NOT NULL PRIMARY KEY,
   nome character varying(150),
   email character varying(100),
   senha character varying(20),
-  data_cadastro date default now()
+  data_cadastro datetime default now()
 );
 
-CREATE TABLE financas.lancamento
+drop table usuario;
+
+CREATE TABLE lancamento
 (
-  id bigserial NOT NULL PRIMARY KEY ,
+  id int auto_increment NOT NULL PRIMARY KEY ,
   descricao character varying(100) NOT NULL,
   mes integer NOT NULL,
   ano integer NOT NULL,
   valor numeric(16,2) NOT NULL,
   tipo character varying(20) CHECK (tipo in ('RECEITA', 'DESPESA') ) NOT NULL,
   status character varying(20) CHECK (status in ( 'PENDENTE', 'CANCELADO', 'EFETIVADO') ) NOT NULL,
-  id_usuario bigint REFERENCES financas.usuario (id) NOT NULL,
-  data_cadastro date default now()
+  id_usuario int REFERENCES financas.usuario (id),
+  data_cadastro datetime default now()
 );
